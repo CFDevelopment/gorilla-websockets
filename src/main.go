be7952a787ca9f2abb04b3e6b1efcdb28dfc21a9
Chris Fenos <chris.m.fenos@gmail.com>
2019-04-03 18:45:16 -0400

    rpcConfig.json initial structure and setup. Typed interface will be created for any given unique json rpc call. TODO: divide all the json objects by rpc method into their own files. Rpc calls are selected, the files are then merged to reduce the amount of unused structs introduced into memory. Need to determine the dependencies of any given method based on the subset of methods selected. The goal is to have the least amount of request to the node in order to maximize server performance.

--- src/main.go
+++ src/main.go
@@ -5,18 +5,16 @@ import (
 	"encoding/json"
 	"log"
 	"strings"
-
 	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 
-	// "github.com/ethereum/go-ethereum/ethclient"
 	"github.com/gorilla/websocket"
 )
 
-// upgrade original http request to ws
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
@@ -31,26 +29,27 @@ type Call struct {
 	Id      string   `json:"id"`
 }
 
-/**
-	1) setup client will return your client and node endpoint
-	2) setup call structs (parses json rpc strings into above call struct)
-	3) rpcCallPulse executes each of the rpc methods through a generic rpc execution function
-	NOTE: any rpc call you need can be setup in the setupRpcCalls function
-	TODO: refactor rpcCallPulse function with go routines, channels & async.WaitGroups to handle all rpc calls
- */
+// 													updated notes... 												  //
+// - rpcConfig.json will contain the typed rpc call and dependency objects in json format.
+// - The user can dynamically subscribe to any subset of rpc api calls via command line or with our custom web
+// 	 interface.
+// - Use the --cmd flag on launch to default node setup localhost:8454
+// - our custom rpc type config allows for a dynamic and resource efficient query system.
+// 																	 												  //
+// 																	 												  //
 func main() {
-	client, endpoint := setupClient()
-	rpcCallStructs := setupRpcCalls()
-	rpcCallPulse(client, endpoint, rpcCallStructs)
+	client, endpoint, rpcDependencies, rpcCallStructs := initNode() // all basic setup is done here
+	rpcCallPulse(client, endpoint, rpcDependencies, rpcCallStructs) // call this to generate node stats
 	fmt.Println(rpcCallStructs)
 }
 
-/*
-	setupClient will boot either the gui or command line pre config option based on the flag that is passed in to run
-	this application. --cmd || --gui respectively. If --cmd flag is passed, the user will be prompted to enter a series
-	of http endpoints with rpc enabled clients. This will allow a user to register one or many nodes with our network
-	stats client.
- */
+func initNode() (http.Client, string, map[string][]string, []Call){
+	client, endpoint := setupClient()
+	rpcCallStructs := setupRpcCalls()
+	rpcDependencies := defineRpcDependencies()
+	return client, endpoint, rpcDependencies, rpcCallStructs
+}
+
 func setupClient() (http.Client, string) {
 	cmdLinePtr := flag.Bool("cmd", false, "should we boot without gui")
 	guiPtr := flag.Bool("gui", false, "should we boot with gui")
@@ -76,8 +75,6 @@ func setupClient() (http.Client, string) {
 	return http.Client{}, endpoint
 }
 
-// additional option to listen in on multiple endpoint for various clients.
-// this will allow other developers to setup their own cluster of nodes with custom UI
 func setupClientEndpoints (setupOption string) string {
 	var endpoint string
 	switch setupOption {
@@ -97,10 +94,6 @@ func setupClientEndpoints (setupOption string) string {
 	return endpoint
 }
 
-/**
-	parseCmdLineEndpoints will set the endpoint for the json rpc connection.
-	TODO: make continuous prompt/currently hardcoded to accept only one value...
- */
 func parseCmdLineEndpoint() string {
 	var response string
 	fmt.Println("---                                  PROMPT                                      ---")
@@ -116,19 +109,19 @@ func parseCmdLineEndpoint() string {
 	return response
 }
 
-
 func setupRpcCalls() []Call {
 	// list of rpc strings we will be using
 	rpcStrings := []string{
 		`{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":"74"}`,
 		`{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":"1"}`,
+		`{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["eth_blockNumber", "true"],"id":1}`,
+		//`{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["eth_blockNumber", true],"id":1}`, //first inp = res (blocknum)
 	}
 	// parse these strings into structs
 	rpcStructs := initializeCallStructs(rpcStrings)
 	return rpcStructs
 }
 
-// will be called once in setup
 func initializeCallStructs (rpcStrings []string) []Call {
 	callSlice := []Call{}
 	for i := 0; i <len(rpcStrings); i++ {
@@ -144,29 +137,65 @@ func initializeCallStructs (rpcStrings []string) []Call {
 	return callSlice
 }
 
-/**
-	Will iterate through call structs & execute their rpc methods
-	TODO: Refactor to go routines and channels
- */
-func rpcCallPulse(client http.Client, url string, calls []Call) {
+func rpcCallPulse(client http.Client, url string, rpcDependencies map[string][]string, calls []Call) {
 	// loop through calls & execute each rpc method
+	// creates method => response mapping to lookup results if needed by other functions as params
+	respMapping := make(map[string]Resp)
 	for i := 0; i < len(calls); i++ {
-		executeRpcCall(client, url, calls[i])
-	}
+		// should add a middleware to check for needed inputs (ex -> block number => get block by number(block num))
+		// if params length = 0 no check is needed
+		// if there are params, check if the function can be executed independently or needs input from other func input
+		if len(calls[i].Params) == 0 {
+			fmt.Println("0 params")
+			resp, err := executeRpcCall(client, url, calls[i])
+			// will check it with our mapping, if it has not been done yet, add it to queue & then execute at end
+			// have a basic alg that dynamically executes all methods
+			// the end result will be registering all the rpc methods you want, adding their predecessors and execute
+			if err != nil {
+				fmt.Println(err)
+			}
 
+			respMapping[calls[i].Method] = resp
+		} else {
+			fmt.Println("more than 0 params")
+			parseArrayToString(calls[i].Params)
+			// checkRpcDependencies(rpcDependencies, respMapping, calls[i])
+			// if the dep has not been executed => put in queue
+		}
+	}
+	fmt.Println("respMapping", respMapping)
 }
 
-func executeRpcCall(client http.Client, url string, call Call) (string, error) {
+func defineRpcDependencies() map[string][]string {
+	// eth_getBlockByNumber needs eth_blockNumber result
+	// setup any future method param dependencies here
+	var rpcDependencies = map[string][]string{}
+	rpcDependencies["eth_getBlockByNumber"] = []string{"eth_blockNumber"}
+	return rpcDependencies
+}
+
+/**
+	checkRpcDependencies is used to determine if a given rpc method being executed in any given go routine needs
+	another method's output as input for the current param array. The dependencies mapping can lookup a method and if
+	they require other methods to be invoked. The respMapping maps a method to a rpc response struct, which will be used
+	to indicate what rpc methods have been invoked.
+ */
+func checkRpcDependencies(dependencies map[string][]string, respMapping map[string]Resp, call Call) {
+	// want to actually change the given call object to alter the params in the struct
+}
+
+func executeRpcCall(client http.Client, url string, call Call) (Resp, error) {
 	paramString := parseArrayToString(call.Params)
-	fmt.Println(call.Method, call.Jsonrpc, call.Params)
+	fmt.Println("param string [executeRpcCall]", paramString)
 	jsonStr := `{"jsonrpc":"`+ call.Jsonrpc +`","method":"`+ call.Method +`","params":[`+ paramString +`],"id":`+ call.Id +`}`
+	fmt.Println("jsonString:", jsonStr)
 	jsonBytes := []byte(jsonStr)
 
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonBytes))
 
 	if err != nil {
 		fmt.Println("[error] RPC post request")
-		return "", errors.New("[error] RPC post request")
+		return nil, errors.New("[error] RPC post request")
 	}
 
 	req.Header.Set("Content-Type", "application/json")
@@ -174,7 +203,7 @@ func executeRpcCall(client http.Client, url string, call Call) (string, error) {
 
 	if err != nil {
 		fmt.Println("[error] RPC post request")
-		return "", errors.New("[error] RPC post request")
+		return nil, errors.New("[error] RPC post request")
 	}
 
 	defer resp.Body.Close()
@@ -183,23 +212,42 @@ func executeRpcCall(client http.Client, url string, call Call) (string, error) {
 
 	if err != nil {
 		fmt.Println("[error] IO read all error")
-		return "", errors.New("[error] IO read all error")
+		return nil, errors.New("[error] IO read all error")
 	} else {
 		fmt.Println("[ body - result ]")
 		fmt.Println(string(body))
 	}
 
-	return string(body), nil
+	r := new(Resp)
+	err = json.Unmarshal([]byte(body), r)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("response", r)
+
+	return *r, nil
 }
 
 /**
-	parseArrayToString will take a param array and return string delimited by comma for json rpc param string
+	parseArrayToString will take a json rpc string and format it such that it will unqoute bool values and
+	add quotes to non
+	TODO: refactor for functions
  */
 func parseArrayToString (params []string) string {
+	formattedInput := make([]string, len(params))
 	if len(params) == 0 {
 		return ""
 	}
-	justString := strings.Join(params,",")
+	for i := 0; i < len(params); i++ {
+		if strings.EqualFold("true", params[i]) != true && strings.EqualFold("false", params[i]) != true {
+			formattedInput = append(formattedInput, strconv.Quote(params[i]))
+		} else {
+			formattedInput = append(formattedInput, params[i])
+		}
+	}
+ 	justString := strings.Join(formattedInput,",")
 	return justString
 }
 
